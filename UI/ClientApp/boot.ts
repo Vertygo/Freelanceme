ea5d2ce30b7b138d3adce3ff3997b782f265574e
Ivan Milosavljevic <ivan.milosavljevic@leversys.com>
2017-11-07 11:20:53 +0000

    init

---
+++ UI/ClientApp/boot.ts
@@ -0,0 +1,38 @@
+import 'core-js/fn/promise';
+import 'core-js/fn/object/assign';
+
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/set';
+
+import './css/site.css';
+import 'bootstrap';
+import Vue from 'vue';
+//import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+//Vue.use(VueResource);
+
+const routes = [
+    { path: '/', component: require('./components/home/home.vue.html') },
+    { path: '/counter', component: require('./components/counter/counter.vue.html') },
+    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
+];
+
+new Vue({
+    el: '#app-root',
+    router: new VueRouter({ mode: 'history', routes: routes }),
+    render: h => h(require('./components/app/app.vue.html'))
+});
---
+++ UI/ClientApp/components/fetchdata/fetchdata.ts
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+interface WeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
+
+@Component
+export default class FetchDataComponent extends Vue {
+    forecasts: WeatherForecast[] = [];
+
+    mounted() {
+        fetch('api/SampleData/WeatherForecasts')
+        //this.$http.get('api/SampleData/WeatherForecasts')
+            .then(response => response.json() as Promise<WeatherForecast[]>)
+            .then(data => {
+                this.forecasts = data;
+            });
+    }
+}
